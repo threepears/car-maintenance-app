76b74c05e5b80c1f5526fbb43dd182aea0a98938
Michael Conrad <michael@threepears.com>
2018-07-14 13:07:50 -0500

    installations and such; clean up of code; add config

--- www/src/containers/App.js
+++ www/src/containers/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Button from '@material-ui/core/Button'
 
+import { withStyles } from '@material-ui/core/styles'
 
-export default class App extends Component {
+// We can inject some CSS into the DOM.
+const styles = {
+  button1: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+};
+
+
+class App extends Component {
   render() {
     return (
       <div className='butt'>
-        <h1>Hello ReactJs</h1>
-        <Button variant="contained" color="primary">
-          Hello World
+        <h1>Login Below</h1>
+        <Button className='button1' variant="contained" color="primary">
+          Login
         </Button>
-        wut
       </div>
     )
   }
 }
+
+export default withStyles(styles)(App)
--- www/src/index.js
+++ www/src/index.js
@@ -3,5 +3,4 @@ import { render } from 'react-dom'
 import App from './containers/App'
 import './styles/main.scss'
 
-console.log("IN HERE bruh BRUH")
 render(<App/>, document.getElementById('root'))
