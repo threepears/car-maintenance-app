a5bfb5067ce4057c27c74d95b0a7b6cf16b0088c
Michael Conrad <michael@threepears.com>
2018-09-29 23:46:38 -0500

    add Express Router

--- server/index.js
+++ server/index.js
@@ -2,43 +2,51 @@
 
 const express = require('express');
 const app = express();
+const router = express.Router()
+
 const PORT = 5000;
 
-app.get('/service-events', (req, res) => {
+router.get('/', (req, res) => {
+  res.send('Welcome to the auto maintenance app!');
+});
+
+router.get('/service-events', (req, res) => {
   res.send('Here are some service events!');
 });
 
-app.get('/service-categories', (req, res) => {
+router.get('/service-categories', (req, res) => {
   res.send('Here are some service categories!');
 });
 
-app.get('/service-shops', (req, res) => {
+router.get('/service-shops', (req, res) => {
   res.send('Here are some service shops!');
 });
 
-app.get('/users/:userId/profile', (req, res) => {
+router.get('/users/:userId/profile', (req, res) => {
   res.send('Here is your user profile!');
 });
 
-app.get('/users/:userId/vehicles', (req, res) => {
+router.get('/users/:userId/vehicles', (req, res) => {
   res.send('Here is a list of vehicles for a user!');
 });
 
-app.get('/users/:userId/vehicles/:vehicleId', (req, res) => {
+router.get('/users/:userId/vehicles/:vehicleId', (req, res) => {
   res.send('Here is a single vehicle for a user!');
 });
 
-app.get('/users/:userId/service-shops', (req, res) => {
+router.get('/users/:userId/service-shops', (req, res) => {
   res.send('Here is a list of service shops for a user!');
 });
 
-app.get('/users/:userId/service-events', (req, res) => {
+router.get('/users/:userId/service-events', (req, res) => {
   res.send('Here is a list of service events for a user!');
 });
 
-app.get('/users/:userId/service-events/:serviceEvent', (req, res) => {
+router.get('/users/:userId/service-events/:serviceEvent', (req, res) => {
   res.send('Here is a single service event for a user!');
 });
+
+app.use('/', router)
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
