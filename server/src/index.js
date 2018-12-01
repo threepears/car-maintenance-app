da5a0f0fbd25de13d010e686387c36a7d33cefe9
bbleds <bledsoedesigns@gmail.com>
2018-11-30 23:17:42 -0600

    Adding npm cache settings so we only install new packages at build time in docker

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,12 @@
 'use strict'
 
 import express from 'express'
-import q from 'jquery'
 
 const app = express()
 const router = require('./routes')
 const PORT = 5000
 
 app.use('/', router)
-console.log('*************************', q)
 const environment = process.env.NODE_ENV || 'development'    // if something else isn't setting ENV, use development
 const configuration = require('../../knexfile')[environment]    // require environment's settings from knexfile
 const knex = require('knex')(configuration)              // connect to DB via knex using env's settings
