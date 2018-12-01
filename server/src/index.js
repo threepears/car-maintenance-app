6d03d82a32a1f5425392fc554b289ec74775b8d0
bbleds <bledsoedesigns@gmail.com>
2018-11-30 22:59:26 -0600

    Adding build step to always do and npm install when starting dev. This is so that docker container will have all current deps installed

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,19 @@
 'use strict'
 
 import express from 'express'
+import q from 'jquery'
 
 const app = express()
 const router = require('./routes')
 const PORT = 5000
 
 app.use('/', router)
-console.log('*************************')
+console.log('*************************', q)
 const environment = process.env.NODE_ENV || 'development'    // if something else isn't setting ENV, use development
 const configuration = require('../../knexfile')[environment]    // require environment's settings from knexfile
 const knex = require('knex')(configuration)              // connect to DB via knex using env's settings
 
-console.log('RES ****************', knex('users').select('*').then(data =>  console.log('data*************', data)))
+// console.log('RES ****************', knex('users').select('*').then(data =>  console.log('data*************', data)))
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
