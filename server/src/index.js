4d25f04c99f92e81bc8e7a3b1a0dca5fb8dc907b
bbleds <bledsoedesigns@gmail.com>
2018-11-30 22:03:02 -0600

    Running api, front-end, and post gres via docker compose (including front-end hot reload)

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const PORT = 5000
 
 app.use('/', router)
 
-console.log('***')
+console.log('*************************')
 const environment = process.env.NODE_ENV || 'development'    // if something else isn't setting ENV, use development
 const configuration = require('../../knexfile')[environment]    // require environment's settings from knexfile
 const knex = require('knex')(configuration)              // connect to DB via knex using env's settings
